22dcfb1958b73c5e748e3acc71473fd723ca9679
ikenna nwaka <inwaka39@gmail.com>
2025-06-22 17:30:36 +0200

    cancelBtn

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ const confirmCloseDialog = document.getElementById("confirm-close-dialog");
 const openTaskFormBtn = document.getElementById("open-task-form-btn");
 const closeTaskFormBtn = document.getElementById("close-task-form-btn");
 const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
+const cancelBtn = document.getElementById("cancel-btn");

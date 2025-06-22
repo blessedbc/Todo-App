5f53da82b822b200df38030cae7acaf3cc2bf066
ikenna nwaka <inwaka39@gmail.com>
2025-06-22 17:23:44 +0200

    closeTaskFormBtn

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const taskForm = document.getElementById("task-form");
 const confirmCloseDialog = document.getElementById("confirm-close-dialog");
 const openTaskFormBtn = document.getElementById("open-task-form-btn");
+const closeTaskFormBtn = document.getElementById("close-task-form-btn");

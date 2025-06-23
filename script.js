5cfc5a3e04dfdb006b51ec82f6cb0e0378a2c43b
ikenna nwaka <inwaka39@gmail.com>
2025-06-23 20:03:01 +0200

    titleInput

--- script.js
+++ script.js
@@ -6,3 +6,4 @@ const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
 const cancelBtn = document.getElementById("cancel-btn");
 const discardBtn = document.getElementById("discard-btn");
 const tasksContainer = document.getElementById("tasks-container");
+const titleInput = document.getElementById("title-input");

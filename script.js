c68dc4dc783856f52396c202a5388bf9bcf0f7aa
ikenna nwaka <inwaka39@gmail.com>
2025-06-24 18:11:53 +0200

    taskObj

--- script.js
+++ script.js
@@ -23,3 +23,9 @@ const addOrUpdateTask = () => {
     return;
   }
   const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+    const taskObj = {
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    title: titleInput.value,
+    date: dateInput.value,
+    description: descriptionInput.value,
+  };

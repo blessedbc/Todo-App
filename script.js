ab4e9e3abde99f7a2645631eb10ec712a4ed2404
ikenna nwaka <inwaka39@gmail.com>
2025-06-24 18:15:21 +0200

    buttonEl

--- script.js
+++ script.js
@@ -63,3 +63,7 @@ const deleteTask = (buttonEl) => {
   const dataArrIndex = taskData.findIndex(
     (item) => item.id === buttonEl.parentElement.id
   );
+    buttonEl.parentElement.remove();
+  taskData.splice(dataArrIndex, 1);
+  localStorage.setItem("data", JSON.stringify(taskData));
+}

61c63503efd8c99b02bb9bf573d7e5ed6614d588
ikenna nwaka <inwaka39@gmail.com>
2025-06-24 18:11:11 +0200

    addOrUpdateTask

--- script.js
+++ script.js
@@ -16,3 +16,10 @@ let currentTask = {};
 const removeSpecialChars = (val) => {
   return val.trim().replace(/[^A-Za-z0-9\-\s]/g, '')
 }
+
+const addOrUpdateTask = () => {
+   if(!titleInput.value.trim()){
+    alert("Please provide a title");
+    return;
+  }
+  const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
